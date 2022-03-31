0677cc64586d057159b67e51b98f9462e221a585
thetheu <matheus_rp25@hotmail.com>
2022-03-31 20:25:35 -0300

    Basic Structure 1.1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,14 @@
-import { Box, Text, Button } from '@chakra-ui/react'
+import { Box, Text, Button, Heading } from '@chakra-ui/react'
 
 function HomePage() {
   return (
     <Box>
-      <Text>Crie seu persongem</Text>
-      <Text>Criar um personagem do zero no Fábulas & Goblins é muito fácil, basta criar uma conta e gerar
+      <Heading mb={4}>Crie seu persongem</Heading>
+      <Text fontSize='x1'>Criar um personagem do zero no Fábulas & Goblins é muito fácil, basta criar uma conta e gerar
         seus personagens com poucos cliques!</Text>
       <Box>
-        <Button>Crie sua Conta!</Button>
-        <Button>Entrar</Button>
+        <Button size='lg' colorScheme='blue' mt='24px'>Crie sua Conta!</Button>
+        <Button size='lg' colorScheme='blue' mt='24px'>Entrar</Button>
       </Box>
     </Box>
   )
