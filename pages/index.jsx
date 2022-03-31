141cbf56950472e2b2c8e63fdf42b5c85ed8cfc1
thetheu <matheus_rp25@hotmail.com>
2022-03-31 20:15:55 -0300

    Basic Structure

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
+import { Box, Text, Button } from '@chakra-ui/react'
+
 function HomePage() {
-  return <div>Primeiro commit na branch!</div>
+  return (
+    <Box>
+      <Text>Crie seu persongem</Text>
+      <Text>Criar um personagem do zero no Fábulas & Goblins é muito fácil, basta criar uma conta e gerar
+        seus personagens com poucos cliques!</Text>
+      <Box>
+        <Button>Crie sua Conta!</Button>
+        <Button>Entrar</Button>
+      </Box>
+    </Box>
+  )
 }
 
 export default HomePage
