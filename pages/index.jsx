b353d30c722d6bc42eaea115e10c4287dc9fc98e
thetheu <matheus_rp25@hotmail.com>
2022-03-31 19:24:27 -0300

    chakra add

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 function HomePage() {
-    return <div>Primeiro commit na branch!</div>
+  return <div>Primeiro commit na branch!</div>
 
 
-  }
-  
-  export default HomePage
+}
+
+export default HomePage
