9e3ff669856c186541c6fcf08af4f3471e2ab22e
thetheu <matheus_rp25@hotmail.com>
2022-04-03 22:31:24 -0300

    Some stuff

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
 import Button from './components/button'
-import Display from './components/Display'
+import Display from './components/display'
+
 
 function HomePage() {
   return (
     <div>
       <Display mb={4}>Crie seu persongem</Display>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <p>Criar um personagem do zero no Fábulas & Goblins é muito fácil, basta criar uma conta e gerar
         seus personagens com poucos cliques!</p>
       <div>
