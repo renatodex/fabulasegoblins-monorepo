c11390029f6ce75d6f4d50b6797fa5f0d1415bc9
thetheu <matheus_rp25@hotmail.com>
2022-04-03 22:14:32 -0300

    Removing chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
