d0fb5eb55437efe31f233769674147171f16c55f
thetheu <matheus_rp25@hotmail.com>
2022-04-06 19:52:33 -0300

    first page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import './components/waves'
+import Waves from './components/waves'
+import TendaDoGoblin from './components/tendadogoblin'
+import Flag_br from './components/flag_br'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,11 +11,38 @@ function MyApp({ Component, pageProps }) {
         <div className="h-8 bg-aero-blue"></div>
       </header>
 
-      <main className="text-white mx-6 mt-6">
+      <main className="text-white mt-6">
         <Component {...pageProps} />
       </main>
 
-      <footer>
+      <footer className="mt-10">
+        <Waves></Waves>
+
+        <div className="bg-raisin-black pb-5">
+          <div className="text-center text-white italic">
+            <Flag_br></Flag_br> &nbsp; Português Brasil
+          </div>
+
+          <div>
+            <div className="w-14 m-auto">
+              <TendaDoGoblin></TendaDoGoblin>
+            </div>
+          </div>
+
+          <div className="
+          text-center px-3
+          text-aero-blue italic nt-4
+          ">
+            Home | Livro Digital | Sobre |
+            Ajuda | Contato
+          </div>
+
+          <div className='text-aero-blue
+          italic text-center mt-4'>
+            Todos os direitos reservados.
+            O Fábulas & Goblins é licenciado pela  CC BY-NC 4.0
+          </div>
+        </div>
       </footer>
     </div>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,27 @@
 import Button from './components/button'
 
+
 function HomePage() {
   return (
     <div>
       <img src="/header_logo.png"></img>
 
-      <h1 className="text-4xl font-bold font-serif">Crie seu personagem</h1>
-      <p className="mt-5">
-        Criar um personagem do zero no Fábulas & Goblins é muito fácil, basta criar uma conta e gerar
-        seus personagens com poucos cliques!
-      </p>
+      <div className="mx-6 mt-4">
+        <h1 className="text-4xl font-bold font-serif">Crie seu personagem</h1>
+        <p className="mt-5 text-sm">
+          Criar um personagem do zero no
+          <span className="font-bold"> Fábulas &amp; Goblins </span>
+           é muito fácil, basta criar uma conta e gerar
+          seus personagens com poucos cliques!
+        </p>
 
-      <div>
-        <div className="mt-5">
-          <Button>Crie sua Conta!</Button>
-        </div>
-        <div className="mt-5">
-          <Button>Entrar</Button>
+        <div>
+          <div className="mt-5">
+            <Button>Crie sua Conta!</Button>
+          </div>
+          <div className="mt-5">
+            <Button>Entrar</Button>
+          </div>
         </div>
       </div>
     </div>
