6b29a402ae78793cf465aed8a5bdb5dd16d0e9da
renatodex@gmail.com <renatodex@gmail.com>
2022-08-06 17:38:17 -0300

    Implement Esbuild with React app and routing

---
+++ core/app/javascript/app/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Index () {
+  return (
+    <div>
+      <h1>This is the Index</h1>
+    </div>
+  )
+}
--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "index#index"
+
+  # If contraints are needed:
+  # constraints: { subroute: /(?!admin|login|logout).*/ }
+
+  get '*subroute', to: 'index#index'
+
+  # Wildcard generic rails route for react app
 end
