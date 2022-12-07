61202054012abd226ce4b475b906bc1e7c11bf08
renatodex@gmail.com <renatodex@gmail.com>
2022-12-07 20:53:25 -0300

    Authenticação basica concluida

--- sheet-manager/pages/characters/index.jsx
+++ sheet-manager/pages/characters/index.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import Container from '../components/container';
 import Title from '../components/title'
+import useLogin from '../hooks/use_login'
 
 export default function () {
+  const { ping } = useLogin()
+
+  useEffect(() => {
+    ping()
+  }, [])
+
   return (
     <Container>
       <Title>Meus Personagens</Title>
---
+++ sheet-manager/pages/hooks/use_login.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import Router from 'next/router'
+import { toast } from 'react-toastify'
+
+function setToken (token) {
+  if (typeof window !== 'undefined') {
+    sessionStorage.setItem('token', token)
+  }
+}
+
+function getToken () {
+  if (typeof window !== 'undefined') {
+    return sessionStorage.getItem('token')
+  }
+}
+
+function removeToken () {
+  if (typeof window !== 'undefined') {
+    sessionStorage.removeItem('token')
+  }
+}
+
+async function login (email, password) {
+  const response = await fetch("/api/sessions/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+  })
+
+  const data = await response.json()
+
+  if (response.ok) {
+    setToken(data.user.token)
+    Router.push('/characters')
+    toast.success("Login realizado!")
+  } else {
+    toast.error("Usuário ou senha inválidos.")
+  }
+}
+
+async function ping () {
+  const response = await fetch(`/api/sessions/get?token=${getToken()}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+
+  const data = await response.json()
+
+  if (!response.ok) {
+    removeToken()
+    Router.push('/login')
+  }
+}
+
+export default function useLogin () {
+  const [token, setToken] = useState(getToken())
+
+  return {
+    token,
+    setToken,
+    ping,
+    login,
+  }
+}
