25851cc562ef66a9404887aad1a2c9f077ff1044
thetheu <matheus_rp25@hotmail.com>
2023-09-14 18:18:38 -0300

    update

--- sheet-manager/pages/characters/new/culture.jsx
+++ sheet-manager/pages/characters/new/culture.jsx
@@ -1,35 +1,63 @@
 import Header from '../components/header';
 import Container from '../../components/container';
 import { Title, Subtitle } from '../../components/title'
+import React, { useEffect, useState } from 'react';
 
+export async function gett (){
+      useEffect(() => {
+        const getInfo = async () => {
+          const culture = await fetch('http://localhost:5000/motor_admin/data/cultures', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+          const data = await culture.json()
+        }
+      getInfo()
+    })
+
+}
 
 
 export default function Culture(){
-        return(
-            <Container>
-                <Header/>
-                <div>
-                    <div className='pt-6'><Title>Escolha sua <br/>Cultura</Title></div>
+    const cultures = [
+        {
+            title: 'Filhos de Timéria',
+            permalink: 'filhos-de-timeria'
+        }, 
+        {
+            title: 'Filhos de Eregor',
+            permalink: 'filhos-de-eregor'
+        }, 
+        {
+            title: 'Filhos de Arcádia',
+            permalink: 'filhos-de-arcadia'
+        }
+    ];
 
-                    <div class="grid grid-cols-2 gap-4">
-                        <div className='mt-3 content-evenly '>
-                            <img src="/avatar.png" className='inline-block'></img>
-                            <p className='text-base font-serif'>Filhos da <br/>Floresta</p>
-                        </div>
-                        <div className='mt-3 text-'>
-                            <img src="/avatar.png" className='inline-block'></img>
-                            <p className='text-base font-serif'>Filhos da <br/>Floresta</p>
-                        </div>
-                        <div className='mt-3 text-'>
-                            <img src="/avatar.png" className='inline-block'></img>
-                            <p className='text-base font-serif'>Filhos da <br/>Floresta</p>
-                        </div>
-                        <div className='mt-3 text-'>
-                            <img src="/avatar.png" className='inline-block'></img>
-                            <p className='text-base font-serif'>Filhos da <br/>Floresta</p>
-                        </div>
-                    </div>
-                </div>            
-            </Container>  
+    console.log(cultures);
+
+    return(
+        <Container>
+            <Header/>
+            <div>
+                <div className='pt-6'><Title>Escolha sua <br/>Cultura</Title></div>
+
+                <div className="grid grid-cols-2 gap-4">
+                    {
+                        cultures.map(function bring(culture){
+                            return(
+                            <div className='mt-3 content-evenly '>
+                            <img src={`/cultures/${culture.permalink}.png`} className='inline-block'></img>
+                            <p className='text-base font-serif'>{culture.title}</p>
+                            </div>
+                        )
+                        }) 
+                    }
+
+                </div>
+            </div>            
+        </Container>  
     );
 }
