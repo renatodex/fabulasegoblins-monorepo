10c3ba9eb096105f2fe5d9751814bef106560a8c
renatodex@gmail.com <renatodex@gmail.com>
2024-05-23 20:11:19 -0300

    Remove comments

--- core/app/services/sync_spells_from_book.rb
+++ core/app/services/sync_spells_from_book.rb
@@ -169,30 +169,3 @@ class SyncSpellsFromBook
     Grimo.where("permalink LIKE ?","%#{string}%").first
   end
 end
-
-#  id                   :bigint           not null, primary key
-#  book_url             :text
-#  cast_distance        :string
-#  cast_distance_number :integer
-#  duration_time        :string
-#  duration_time_number :integer
-#  icon                 :string
-#  long_description     :text
-#  magic_cost           :string
-#  magic_cost_number    :integer
-#  permalink            :string
-#  range_amount         :string
-#  sacrifice            :boolean
-#  short_description    :text
-#  tags                 :string
-#  tier                 :integer
-#  title                :string
-#  ultimate             :boolean
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  action_type_id       :bigint           not null
-#  attack_logic_id      :bigint           not null
-#  element_id           :bigint           not null
-#  external_id          :string
-#  range_type_id        :bigint           not null
-#
