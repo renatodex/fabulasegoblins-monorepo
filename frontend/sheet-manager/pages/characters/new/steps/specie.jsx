3a7b0e385bd7b193c93c421e191cfbdf2be8fc13
renatodex@gmail.com <renatodex@gmail.com>
2024-07-21 06:28:30 -0300

    Remove console log

--- frontend/sheet-manager/pages/characters/new/steps/specie.jsx
+++ frontend/sheet-manager/pages/characters/new/steps/specie.jsx
@@ -61,7 +61,6 @@ export default function Specie ({ character, setCharacter }) {
 
         <Button
             onClick={e => {
-              console.log(character.details)
               setCharacter({
                 ...character,
               })
